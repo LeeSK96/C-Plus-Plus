a174acf2b533631fa6f8317417d07d9fa3b3daa7
EC2 Default User <ec2-user@ip-172-31-13-145.us-east-2.compute.internal>
2021-11-30 04:03:44 +0000

    11/30 승규 작업 linked list  파일수정

--- data_structures/linked_list.cpp
+++ data_structures/linked_list.cpp
@@ -18,144 +18,67 @@
 #include <memory>
 #include <string>
 
-/**
- * @namespace data_structures
- * @brief Data Structures algorithms
- */
-namespace data_structures {
-
-/**
- * @namespace linked_list
- * @brief Functions for singly linked list algorithm
- */
-namespace linked_list {
-
-/**
- * This function checks if the string passed consists
- * of only digits.
- * @param s To be checked if s contains only integers
- * @returns true if there are only digits present in the string
- * @returns false if any other character is found
- */
-bool isDigit(const std::string& s) {
-    // function statements here
-    for (char i : s) {
-        if (!isdigit(i)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-/**
- * A link class containing a value and pointer to another link
- */
-class link {
- private:
-    int pvalue;                   ///< value of the current link
-    std::shared_ptr<link> psucc;  ///< pointer to the next value on the list
-
- public:
-    /**
-     * function returns the integer value stored in the link.
-     * @returns the integer value stored in the link.
-     */
-    int val() { return pvalue; }
-
-    /**
-     * function returns the pointer to next link
-     * @returns the pointer to the next link
-     * */
-    std::shared_ptr<link>& succ() { return psucc; }
-
-    /**
-     * Creates link with provided value and pointer to next link
-     * @param value is the integer stored in the link
-     */
-    explicit link(int value = 0) : pvalue(value), psucc(nullptr) {}
-};
-
-/**
- * A list class containing a sequence of links
- */
-class list {
- private:
-    std::shared_ptr<link> first;  ///< link before the actual first element
-    std::shared_ptr<link> last;   ///< last link on the list
- public:
-    /**
-     * List constructor. Initializes the first and last link.
-     */
-    list() {
-        // Initialize the first link
-        first = std::make_shared<link>();
-        // Initialize the last link with the first link
-        last = nullptr;
-    }
-
-    bool isEmpty();
-
-    void push_back(int new_elem);
-    void push_front(int new_elem);
-    void erase(int old_elem);
-    void display();
-    std::shared_ptr<link> search(int find_elem);
-    void reverse();
-};
+#include "data_structure.hpp"
 
 /**
  * function checks if list is empty
  * @returns true if list is empty
  * @returns false if list is not empty
  */
-bool list::isEmpty() {
+template <typename T>
+bool data_structures::list<T>::isEmpty() {
     if (last == nullptr) {
         return true;
     } else {
         return false;
     }
 }
-
 /**
  * function adds new element to the end of the list
  * @param new_elem to be added to the end of the list
  */
-void list::push_back(int new_elem) {
+template <typename T>
+void data_structures::list<T>::push_back(T new_elem) {
     if (isEmpty()) {
-        first->succ() = std::make_shared<link>(new_elem);
+        first->succ() = std::make_shared<link<T> >(new_elem);
         last = first->succ();
     } else {
-        last->succ() = std::make_shared<link>(new_elem);
+        last->succ() = std::make_shared<link<T> >(new_elem);
         last = last->succ();
     }
+    list_size++;
 }
 
 /**
  * function adds new element to the beginning of the list
  * @param new_elem to be added to front of the list
  */
-void list::push_front(int new_elem) {
+template <typename T>
+void data_structures::list<T>::push_front(T new_elem) {
     if (isEmpty()) {
-        first->succ() = std::make_shared<link>(new_elem);
+        first->succ() = std::make_shared<link<T> >(new_elem);
         last = first->succ();
     } else {
-        std::shared_ptr<link> t = std::make_shared<link>(new_elem);
+        std::shared_ptr<data_structures::link<T> > t =
+            std::make_shared<link<T> >(new_elem);
         t->succ() = first->succ();
         first->succ() = t;
     }
+    list_size++;
 }
 
 /**
  * function erases old element from the list
  * @param old_elem to be erased from the list
  */
-void list::erase(int old_elem) {
+template <typename T>
+void data_structures::list<T>::erase(T old_elem) {
     if (isEmpty()) {
         std::cout << "List is Empty!";
         return;
     }
-    std::shared_ptr<link> t = first;
-    std::shared_ptr<link> to_be_removed = nullptr;
+    std::shared_ptr<link<T> > t = first;
+    std::shared_ptr<link<T> > to_be_removed = nullptr;
     while (t != last && t->succ()->val() != old_elem) {
         t = t->succ();
     }
@@ -169,50 +92,54 @@ void list::erase(int old_elem) {
     if (t->succ() == nullptr) {
         last = t;
     }
-    if (first == last){
+    if (first == last) {
         last = nullptr;
     }
+    list_size--;
 }
 
 /**
  * function displays all the elements in the list
  * @returns 'void'
  */
-void list::display() {
+template <typename T>
+void data_structures::list<T>::print_list() {
     if (isEmpty()) {
         std::cout << "List is Empty!";
         return;
     }
-    std::shared_ptr<link> t = first;
+    std::shared_ptr<link<T> > t = first;
     while (t->succ() != nullptr) {
-        std::cout << t->succ()->val() << "\t";
+        std::cout << t->succ()->val() << " ";
         t = t->succ();
     }
+    std::cout << std::endl;
 }
 
 /**
  * function searchs for @param find_elem in the list
  * @param find_elem to be searched for in the list
+ * @return index of find_elem
  */
-std::shared_ptr<link> list::search(int find_elem) {
-    if (isEmpty()) {
+template <typename T>
+int data_structures::list<T>::search(T find_elem) {
+    int idx = 0;
+    if (isEmpty() == true) {
         std::cout << "List is Empty!";
-        return nullptr;
+        return -1;
     }
-    std::shared_ptr<link> t = first;
+    std::shared_ptr<link<T> > t = first;
     while (t != last && t->succ()->val() != find_elem) {
         t = t->succ();
+        idx++;
     }
     if (t == last) {
         std::cout << "Element not found\n";
-        return nullptr;
+        return -1;
     }
     std::cout << "Element was found\n";
-    return t->succ();
+    return idx;
 }
-}  // namespace linked_list
-}  // namespace data_structures
-
 /**
  * Main function:
  * Allows the user add and delete values from the list.
@@ -220,59 +147,64 @@ std::shared_ptr<link> list::search(int find_elem) {
  * @returns 0 on exit
  */
 int main() {
-    data_structures::linked_list::list l;
-    int choice = 0;
-    int x = 0;
-    std::string s;
-    do {
-        std::cout << "\n1. Insert";
-        std::cout << "\n2. Delete";
-        std::cout << "\n3. Search";
-        std::cout << "\n4. Print";
-        std::cout << "\n0. Exit";
-        std::cout << "\n\nEnter you choice : ";
-        std::cin >> choice;
-        switch (choice) {
-            case 1:
-                std::cout << "\nEnter the element to be inserted : ";
-                std::cin >> s;
+    data_structures::list<int> L_int;
+    data_structures::list<float> L_float;
 
-                if (data_structures::linked_list::isDigit(s)) {
-                    x = std::stoi(s);
-                    l.push_back(x);
-                } else {
-                    std::cout << "Wrong Input!\n";
-                }
-                break;
-            case 2:
-                std::cout << "\nEnter the element to be removed : ";
-                std::cin >> s;
-                if (data_structures::linked_list::isDigit(s)) {
-                    x = std::stoi(s);
-                    l.erase(x);
-                } else {
-                    std::cout << "Wrong Input!\n";
-                }
-                break;
-            case 3:
-                std::cout << "\nEnter the element to be searched : ";
-                std::cin >> s;
-                if (data_structures::linked_list::isDigit(s)) {
-                    x = std::stoi(s);
-                    std::shared_ptr<data_structures::linked_list::link> found =
-                        l.search(x);
-                } else {
-                    std::cout << "Wrong Input!\n";
-                }
-                break;
-            case 4:
-                l.display();
-                std::cout << "\n";
-                break;
-            default:
-                std::cout << "Invalid Input\n" << std::endl;
-                break;
-        }
-    } while (choice != 0);
+    std::cout << "\nTEST integer type\n" << std::endl;
+    // Insert testing
+    L_int.push_back(11);
+    L_int.push_back(12);
+    L_int.push_back(15);
+    L_int.push_back(10);
+    L_int.print_list();
+    assert(L_int.size() == 4);
+
+    L_int.push_front(12);
+    L_int.push_front(20);
+    L_int.push_front(18);
+    assert(L_int.size() == 7);
+    L_int.print_list();  // To print the array
+
+    // Remove testing
+    L_int.erase(12);  // Remove Duplicate value in the list
+    L_int.erase(15);  // Remove the existing value in the list
+    assert(L_int.size() == 5);
+    L_int.erase(50);  // Try to remove the non-existing value in the list
+    assert(L_int.size() == 5);
+    L_int.print_list();
+
+    // LinearSearch testing
+    assert(L_int.search(11) == 2);  // search for the existing element
+    assert(L_int.search(12) == 3);
+    assert(L_int.search(50) == -1);  // search for the non-existing element
+
+    std::cout << "\nTEST float type\n" << std::endl;
+    // Insert testing
+    L_float.push_back(1.1);
+    L_float.push_back(1.42);
+    L_float.push_back(15.3);
+    L_float.push_back(10.1);
+    L_float.print_list();
+    assert(L_float.size() == 4);
+
+    L_float.push_front(0.12);
+    L_float.push_front(-20);
+    L_float.push_front(1.18);
+    assert(L_float.size() == 7);
+    L_float.print_list();  // To print the array
+
+    // Remove testing
+    L_float.erase(0.12);  
+    L_float.erase(15.3);  
+    assert(L_float.size() == 5);
+    L_float.erase(500);  // Try to remove the non-existing value in the list
+    assert(L_float.size() == 5);
+    L_float.print_list();
+
+    // LinearSearch testing
+    assert(L_float.search(1.1) == 2);  // search for the existing element
+    assert(L_float.search(1.42) == 3);
+    assert(L_float.search(50) == -1);  // search for the non-existing element
+
     return 0;
 }
